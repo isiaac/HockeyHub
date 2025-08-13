ce7d838739dfd998a7bc00032031efcd199b01cc
isiaac <irayc1996@gmail.com>
2025-08-12 20:13:12 -0700

    Fix Babel Configuration Completely

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: 'react',
-          jsxRuntime: 'automatic',
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
